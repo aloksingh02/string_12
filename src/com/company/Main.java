34f41a1984a323df1bf464994ccc62e1fb15f8e4
Alok KumarS Singh <singhalokkumar427@gmail.com>
2022-04-01 23:56:36 +0530

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String s1 = "alok singh";
+        String s2 = "alok singh";
+        String s3 = "amit singh";
+        System.out.println(s1.compareTo(s2));
+        System.out.println(s1.compareTo(s3));
+        System.out.println(s3.compareTo(s2));
+	// write your code here
+    }
+}
